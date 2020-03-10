e4277896bab8479bf4bc34bc1a19c77b7909e94b
Jose Campo <josefcc96@gmail.com>
2020-03-10 08:56:42 -0500

    creacion de modelos y relaciones entre ellos

--- apps/datos/admin.py
+++ apps/datos/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Lugar, Especie, Avistamiento
+
+
+class LugarAdmin(admin.ModelAdmin):
+    list_display = ('ID', 'pais', 'departamento', 'nombre', 'alias')
+    list_filter = ('pais', 'departamento', 'nombre', 'alias')
+
+    def ID(self, obj):
+        return '{}'.format(obj.pk)
+    # ordering = ('nombre')
+
+
+class AvistamientoAdmin(admin.ModelAdmin):
+    list_display = ('ID', 'post_especie', 'lugar', 'latitud',
+                    'longitud', 'autor', 'observación')
+    # ordering = ('autor')
+    search_fields = ('lugar__nombre', 'especie__n_comun', 'autor')
+    list_filter = ('especie__n_comun', 'lugar__nombre', 'autor')
+
+    def ID(self, obj):
+        return '{}'.format(obj.pk)
+
+    def post_especie(self, obj):
+        return ' , '.join(
+            [p.get_Esp() for p in obj.especie.all()])
+    post_especie.short_description = "Especies (Nombres Comunes)"
+
+
+class EspecieAdmin(admin.ModelAdmin):
+    list_display = ('ID', 'n_comun', 'n_cientifico', 'familia')
+    list_filter = ('n_comun', 'n_cientifico', 'familia')
+
+    def ID(self, obj):
+        return '{}'.format(obj.pk)
+    # ordering = ('n_comun')
+
+
+admin.site.register(Lugar, LugarAdmin)
+admin.site.register(Especie, EspecieAdmin)
+admin.site.register(Avistamiento, AvistamientoAdmin)
--- apps/datos/models.py
+++ apps/datos/models.py
@@ -1,36 +1,54 @@
 from django.db import models
 
 
-
 class Especie(models.Model):
-    n_comun = models.TextField(max_length=128)
-    n_cientifico = models.TextField(max_length=128)
-    familia = models.TextField(max_length=128)
+    n_comun = models.TextField(max_length=128, verbose_name="Nombre Comun")
+    n_cientifico = models.TextField(
+        max_length=128,  verbose_name="Nombre Cientifico")
+    familia = models.TextField(max_length=128,  verbose_name="Familia")
+
+    class Meta:
+        verbose_name = "Especie"
+        verbose_name_plural = "Especies"
+        ordering = ["-pk"]
+
+    def get_Esp(self):
+        return str("{}".format(self.n_comun))
 
     def __str__(self):
         return self.n_comun
 
 
 class Lugar(models.Model):
-    pais = models.CharField(max_length=128)
-    departamento = models.CharField(max_length=128)
-    nombre = models.CharField(max_length=128)
-    alias = models.CharField(max_length=128)
-   
+    pais = models.CharField(max_length=128,  verbose_name="Pais")
+    departamento = models.CharField(
+        max_length=128, verbose_name="Departamento")
+    nombre = models.CharField(max_length=128,  verbose_name="Nombre")
+    alias = models.CharField(max_length=128,  verbose_name="Alias")
+
+    class Meta:
+        verbose_name = "Lugar"
+        verbose_name_plural = "Lugares"
+        ordering = ["-pk"]
 
     def __str__(self):
         return self.nombre
 
-  
 
 class Avistamiento(models.Model):
-    
-    latitud = models.FloatField()
-    longitud = models.FloatField()
-    autor = models.CharField(max_length=128)
-    observación = models.TextField(null=True,blank=True)
+    latitud = models.FloatField(verbose_name="Latitud")
+    longitud = models.FloatField(verbose_name="Longitud")
+    autor = models.CharField(max_length=128,  verbose_name="Autor")
+    observación = models.TextField(
+        null=True, blank=True, verbose_name="Observaciones")
     especie = models.ManyToManyField(Especie)
-    lugar = models.ForeignKey(Lugar,null=True,blank=True,on_delete=models.CASCADE) 
-    
+    lugar = models.ForeignKey(
+        Lugar, null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Avistamiento"
+        verbose_name_plural = "Avistamientos"
+        ordering = ["-pk"]
+
     def __str__(self):
         return 'ID={},Autor={}'.format(self.pk, self.autor)
