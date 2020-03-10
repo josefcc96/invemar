18e414d8bfcd8003db4af33d3aab7e9a2def69f4
Josefcc96 <josefcc96@gmail.com>
2020-03-10 01:39:32 -0500

    creacion de models

--- apps/datos/models.py
+++ apps/datos/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Especie(models.Model):
+    n_comun = models.TextField(max_length=128)
+    n_cientifico = models.TextField(max_length=128)
+    familia = models.TextField(max_length=128)
+
+    def __str__(self):
+        return self.n_comun
+        
+
+class Lugar(models.Model):
+    pais = models.CharField(max_length=128)
+    departamento = models.CharField(max_length=128)
+    nombre = models.CharField(max_length=128)
+    alias = models.CharField(max_length=128)
+   
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class Avistamiento(models.Model):
+    
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    autor = models.CharField(max_length=128)
+    observación = models.TextField(null=True,blank=True)
+    especie = models.ManyToManyField(Especie)
+    lugar = models.ManyToManyField(Lugar) 
+    
+    def __str__(self):
+        return 'ID={},Autor={}'.format(self.pk, self.autor)
